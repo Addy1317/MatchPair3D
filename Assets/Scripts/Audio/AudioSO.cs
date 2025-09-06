ef9fee41c509f3316b6c243e1b402d06ff3cf5bb
Aditya Singh <92723657+Addy1317@users.noreply.github.com>
2025-09-07 01:03:18 +0530

    feat: Audio
    - AudioManager
    - AudioSO

---
+++ Assets/Scripts/Audio/AudioSO.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+
+namespace SlowpokeStudio.Audio
+{
+    public enum SFXType
+    {
+        OnButtonClickSFX,
+        OnSelectionClickSFX,
+        OnGoalCompletedSFX
+    }
+
+    [CreateAssetMenu(fileName = "AudioSO", menuName = "Audio/AudioSO")]
+    public class AudioSO : ScriptableObject
+    {
+        [System.Serializable]
+        public struct SFXAudio
+        {
+            public SFXType sfxType;
+            public AudioClip audioClip;
+        }
+
+        [Header("Background Music")]
+        [SerializeField] internal AudioClip backgroundMusicClip;
+        [SerializeField][Range(0f, 1f)] internal float backgroundMusicVolume = 0.5f;
+
+        [Header("SFX")]
+        [SerializeField] internal SFXAudio[] sfxClips;
+        [SerializeField][Range(0f, 1f)] internal float sfxVolume = 0.5f;
+
+        internal AudioClip GetSFXClip(SFXType sfxType)
+        {
+            foreach (var sfx in sfxClips)
+            {
+                if (sfx.sfxType == sfxType)
+                {
+                    return sfx.audioClip;
+                }
+            }
+
+            Debug.LogWarning($"SFX clip for {sfxType} not found. Returning null.");
+            return null;
+        }
+    }
+}
+
